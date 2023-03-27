e2f709cee89fb4f6eab09d589081debb9e2d62f8
vsalw <81428968@qq.com>
2023-03-27 14:38:43 +0800

    Update default.ts

--- src/default.ts
+++ src/default.ts
@@ -9,6 +9,6 @@ export const defaultSetting = {
 export const defaultMessage = `
 - 本站点使用 OpenAI API (gpt-3.5-turbo)模型，由开源老王自费搭建，公益性质。
 - 关注公众号[开源老王](https://ruoyi-vsalw.oss-cn-shanghai.aliyuncs.com/image/2023/03/%E5%BC%80%E6%BA%90%E8%80%81%E7%8E%8B_20230309104243A004.jpg)，回复【chatgpt】获取体验key。
-- 点击下面设置图标，填填入API-KEY即可。
-- 访问人数较多时，会报错Error，这时请重新刷新后再提问，你也可以联系[微信wllkeeptrying](https://ruoyi-vsalw.oss-cn-shanghai.aliyuncs.com/image/2023/03/wx_20230321183041A023.png)购买独享账号。
+- 点击下面设置图标，填填入API-KEY即可，搭建这样的站点联系老王。
+- 访问人数较多时，会报错Error，这时请重新刷新后再提问，你也可以联系老王，微信是[wllkeeptrying](https://ruoyi-vsalw.oss-cn-shanghai.aliyuncs.com/image/2023/03/wx_20230321183041A023.png)购买独享账号。
 - <kbd>Shift</kbd> + <kbd>Enter</kbd> 换行。开头输入 <kbd>/</kbd> 或者 <kbd>空格</kbd> 搜索 Prompt 预设。点击输入框滚动到底部。`
