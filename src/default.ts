c6710c5d5640c8e3fa84c616e33aa803b007a506
vsalw <81428968@qq.com>
2023-04-03 10:25:24 +0800

    Update default.ts

--- src/default.ts
+++ src/default.ts
@@ -8,7 +8,7 @@ export const defaultSetting = {
 
 export const defaultMessage = `
 - 本站点使用3.5型，由开源老王自费搭建，公益性质。
-- 4.2日更新：暂时不能体验了，因为老王的账户已经没钱了，官网又暂时关闭了注册...等开放注册老王再去注册个账户，若你想贡献一下联系老王。
+- 4.3日更新：昨天号被封了，今天老王换了新账号，老铁们可以继续体验了。
 - 关注公众号[开源老王](https://ruoyi-vsalw.oss-cn-shanghai.aliyuncs.com/image/2023/03/%E5%BC%80%E6%BA%90%E8%80%81%E7%8E%8B_20230309104243A004.jpg)，回复【666】获取体验key。
 - 点击下面设置图标，填填入API-KEY即可，访问人数较多时，会报错Error，这时请重新刷新后再提问。
 - 若你想搭建这样的站点联系老王。你也可以联系老王，微信是[www_666mmb_com](https://ruoyi-vsalw.oss-cn-shanghai.aliyuncs.com/image/2023/03/开源老王微信号_20230328095001A024.jpg)购买独享账号。
