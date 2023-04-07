b1866802d9b82ab317c313701f2e4b3efd99b7d6
hututuyyds <129722237+hututuyyds@users.noreply.github.com>
2023-04-07 16:52:07 +0800

    Update default.ts

--- src/default.ts
+++ src/default.ts
@@ -7,9 +7,9 @@ export const defaultSetting = {
 }
 
 export const defaultMessage = `
-- 本站点使用3.5型，由开源老王自费搭建，公益性质。
-- 4.3日更新：昨天号被封了，今天老王换了新账号，老铁们可以继续体验了。
-- 关注公众号[开源老王](https://ruoyi-vsalw.oss-cn-shanghai.aliyuncs.com/image/2023/03/%E5%BC%80%E6%BA%90%E8%80%81%E7%8E%8B_20230309104243A004.jpg)，回复【666】获取体验key。
-- 点击下面设置图标，填填入API-KEY即可，访问人数较多时，会报错Error，这时请重新刷新后再提问。
-- 若你想搭建这样的站点联系老王。你也可以联系老王，微信是[www_666mmb_com](https://ruoyi-vsalw.oss-cn-shanghai.aliyuncs.com/image/2023/03/开源老王微信号_20230328095001A024.jpg)购买独享账号。
+- 本站点主要使用gpt-3.5-turbo模型，同时提供gpt-4模型，响应速度更快。
+- 关注QQ号：335088851，回复【体验】获取体验key。
+- 点击设置图标，填填入API-KEY即可，访问人数较多时速度会慢，请耐心等待。
+- 本站点也提供API-KEY购买服务，使用个人独享API-key,响应速度更快。
+- 若你想搭建这样的站点，可以在QQ群向群主联系，我们提供相应服务。
 - 按键盘上<kbd>Shift</kbd> + <kbd>Enter</kbd> 换行。在键盘上开头输入 <kbd>/</kbd> 或者 <kbd>空格</kbd> 搜索 Prompt 预设。点击输入框滚动到底部。`
