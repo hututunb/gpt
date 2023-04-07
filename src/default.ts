5a0f602cd9dd689df0b1bde43b9cf022021168a8
vsalw <81428968@qq.com>
2023-04-07 09:39:57 +0800

    Update default.ts

--- src/default.ts
+++ src/default.ts
@@ -12,4 +12,4 @@ export const defaultMessage = `
 - 关注公众号[开源老王](https://ruoyi-vsalw.oss-cn-shanghai.aliyuncs.com/image/2023/03/%E5%BC%80%E6%BA%90%E8%80%81%E7%8E%8B_20230309104243A004.jpg)，回复【666】获取体验key。
 - 点击下面设置图标，填填入API-KEY即可，访问人数较多时，会报错Error，这时请重新刷新后再提问。
 - 若你想搭建这样的站点联系老王。你也可以联系老王，微信是[www_666mmb_com](https://ruoyi-vsalw.oss-cn-shanghai.aliyuncs.com/image/2023/03/开源老王微信号_20230328095001A024.jpg)购买独享账号。
-- <kbd>Shift</kbd> + <kbd>Enter</kbd> 换行。开头输入 <kbd>/</kbd> 或者 <kbd>空格</kbd> 搜索 Prompt 预设。点击输入框滚动到底部。`
+- 按键盘上<kbd>Shift</kbd> + <kbd>Enter</kbd> 换行。在键盘上开头输入 <kbd>/</kbd> 或者 <kbd>空格</kbd> 搜索 Prompt 预设。点击输入框滚动到底部。`
