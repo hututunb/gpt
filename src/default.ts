887960c9e6ff936398b284e49542e2c87a193990
hututunb <130012637+hututunb@users.noreply.github.com>
2023-04-16 22:10:18 +0800

    Update default.ts

--- src/default.ts
+++ src/default.ts
@@ -8,8 +8,7 @@ export const defaultSetting = {
 
 export const defaultMessage = `
 - 本站点主要使用gpt-3.5-turbo模型，同时提供gpt-4模型，响应速度更快。
-- 关注QQ号：335088851，回复【体验】获取体验key。
+- 关注QQ群(https://user-images.githubusercontent.com/120257354/232317604-23c18048-42e0-4631-a947-51baf2ed230b.png)获取API-KEY
 - 点击设置图标，填填入API-KEY即可，访问人数较多时速度会慢，请耐心等待。
-- 本站点也提供API-KEY购买服务，使用个人独享API-key,响应速度更快。
 - 若你想搭建这样的站点，可以在QQ群向群主联系，我们提供相应服务。
 - 按键盘上<kbd>Shift</kbd> + <kbd>Enter</kbd> 换行。在键盘上开头输入 <kbd>/</kbd> 或者 <kbd>空格</kbd> 搜索 Prompt 预设。点击输入框滚动到底部。`
